25dca0ee1f4f72b7d1bf47cb41cf1c034292441e
Imadrouah <irouah2@gmail.com>
2023-04-30 15:30:46 +0100

    added some features in js file

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const toggleMenu = document.querySelector(".toggle");//btn
-const megaMenu = document.querySelector(".mega-menu");//menu
+const toggleMenu = document.querySelector(".toggle"); //btn
+const other = document.querySelector(".other"); //btn
+const megaMenu = document.querySelector(".mega-menu"); //menu
 
 toggleMenu.addEventListener("click", function () {
-    megaMenu.classList.add("open");
+    megaMenu.classList.toggle("open");
+});
+
+other.addEventListener("click", function () {
+    megaMenu.classList.toggle("open");
 });
 
 document.addEventListener("click", function (event) {
-    if (!megaMenu.contains(event.target) && !toggleMenu.contains(event.target)) {
+    if (!toggleMenu.contains(event.target) && !other.contains(event.target)) {
         megaMenu.classList.remove("open");
     }
 });
